3213b3dee1ce542e1195f47d7b62b0658af8672a
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2020-04-14 01:17:50 +0300

    Elixir: Experimenting with different ways to sum lists

--- languages/elixir/core/basic/lib/basic/testing_elixir.ex
+++ languages/elixir/core/basic/lib/basic/testing_elixir.ex
@@ -8,9 +8,26 @@ defmodule Basic.PlayingWithElixir do
     IO.puts "Hello #{name}!"
   end
 
+  def sum_list([]), do: 0
+  def sum_list([head|tail]) do
+    head + sum_list(tail)
+  end
+
+  def calculate_string_list_sum(string_list) do
+    # IO.puts sum_list(string_list |> Enum.map(&String.to_integer/1))
+    string_list
+    |> Enum.map(&String.to_integer/1)
+    |> sum_list
+  end
+
+  def list_sum_by_reducing(string_list) do
+    string_list
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.reduce(fn x, acc -> acc + x end)
+  end
+
 
   def main(args) do
-    greet(args)
-    :ok
+    IO.puts list_sum_by_reducing(args)
   end
 end
