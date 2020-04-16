a2a5c759b7389009e3d84c3bd913a8687d45fabd
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2020-04-17 00:52:35 +0300

    Elixir: calculating sum of nth sums of a list in 4 epic one-liners. This is so fun...

--- languages/elixir/core/basic/lib/basic/testing_elixir.ex
+++ languages/elixir/core/basic/lib/basic/testing_elixir.ex
@@ -33,8 +33,24 @@ defmodule Basic.PlayingWithElixir do
     |> reduce_int_list_by_sum
   end
 
+  def sum_up_to_nth_element(string_list, n) do
+    string_list
+    |> Enum.slice(0, n + 1)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.reduce(fn x, acc -> acc + x end)
+  end
+
+  def construct_nth_sum_list(string_list) do
+    (for {_number, counter} <- string_list
+                               |> Enum.with_index, do: string_list
+                                                       |> sum_up_to_nth_element(counter))
+  end
+
+  def sum_of_nth_sums_of_list(string_list) do
+    string_list |> construct_nth_sum_list |> sum_int_list
+  end
 
   def main(args) do
-    IO.puts list_sum_by_reducing(args)
+    IO.puts args |> sum_of_nth_sums_of_list
   end
 end
