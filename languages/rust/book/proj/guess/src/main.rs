cd7ead4eea4977cbd5a0a9533342d2100de1fdf3
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2020-11-19 13:36:11 +0200

    Rust: Book: comparing String and Integer in the guessing game.

--- languages/rust/book/proj/guess/src/main.rs
+++ languages/rust/book/proj/guess/src/main.rs
@@ -3,6 +3,7 @@ use rand::Rng;
 
 use std::io::Write;
 use std::io;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number I'm thinking of! The number is between 1 and 100.");
@@ -16,6 +17,9 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read your guess...");
 
-    println!("I'm thinking of this: [{:?}]", secret_number);
-    println!("This is what you guessed: [{:?}]", guess);
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("The number I'm thinking of is smaller!"),
+        Ordering::Greater => println!("The number I'm thinking of is greater!"),
+        Ordering::Equal => println!("Great job! You won, that's exactly the number I'm thinking of!"),
+    }
 }
