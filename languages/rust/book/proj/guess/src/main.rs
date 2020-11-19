e2ae87a5ea96bf839b43c336516ac4adcfe2ed72
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2020-11-19 13:53:00 +0200

    Rust: Book: the guessing game finally looks like a game, with looping until guessed and whatnot.

--- languages/rust/book/proj/guess/src/main.rs
+++ languages/rust/book/proj/guess/src/main.rs
@@ -7,20 +7,26 @@ use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number I'm thinking of! The number is between 1 and 100.");
-    print!("What's your guess? Put it here ==> ");
-    io::stdout().flush().unwrap();
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read your guess...");
-    let guess: u32 = guess.trim().parse().expect("Your guess should be a number.");
+    loop {
+        print!("\nWhat's your guess? Put it here ==> ");
+        io::stdout().flush().unwrap();
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("The number I'm thinking of is smaller!"),
-        Ordering::Greater => println!("The number I'm thinking of is greater!"),
-        Ordering::Equal => println!("Great job! You won, that's exactly the number I'm thinking of!"),
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read your guess...");
+        let guess: u32 = guess.trim().parse().expect("Your guess should be a number.");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("The number I'm thinking of is greater!"),
+            Ordering::Greater => println!("The number I'm thinking of is smaller!"),
+            Ordering::Equal => {
+                println!("Great job! You won, that's exactly the number I'm thinking of!");
+                break;
+            }
+        }
     }
 }
