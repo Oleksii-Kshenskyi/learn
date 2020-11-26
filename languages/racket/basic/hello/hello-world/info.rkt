71c029da965b37a09145420dec34aff539551685
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2020-11-27 00:27:34 +0200

    Racket: freshly generated dummy hello world project.

---
+++ languages/racket/basic/hello/hello-world/info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "hello-world")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/hello-world.scrbl" ())))
+(define pkg-desc "Description Here")
+(define version "0.0")
+(define pkg-authors '(archa))
---
+++ languages/racket/basic/hello/hello-world/scribblings/hello-world.scrbl
@@ -0,0 +1,10 @@
+#lang scribble/manual
+@require[@for-label[hello-world
+                    racket/base]]
+
+@title{hello-world}
+@author{archa}
+
+@defmodule[hello-world]
+
+Package Description Here
