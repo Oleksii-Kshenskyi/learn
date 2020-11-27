53a5e949cb91ccd2e4547af9c25ca044251da547
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2020-11-28 01:50:06 +0200

    Erlang: sum of nth sums: ran a few experiments + calculating sum of the list now.

--- languages/erlang/basic/sumnsums/src/sumnsums.erl
+++ languages/erlang/basic/sumnsums/src/sumnsums.erl
@@ -2,5 +2,13 @@
 
 -export([main/1]).
 
-main(_args) ->
-    io:format("Hello Erlang World!~n").
+strlist_to_intlist(Str_List) ->
+    lists:map(fun (Elem) -> {Num, _} = string:to_integer(Elem), Num end, Str_List).
+
+main(Args) ->
+    A = 3,
+    Int_Args = strlist_to_intlist(Args),
+    io:format("The list of strs from command line: ~p ~n", [Args]),
+    io:format("The list of ints: ~w~n", [Int_Args]),
+    io:format("Sum of all the ints is: ~p!~n", [lists:sum(Int_Args)]),
+    io:format("BTW, the variable is ~p! ~n", [A]).
