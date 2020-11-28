ce05ae0fc479043c7f6f09d80652bdcb951abcf5
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2020-11-28 02:01:38 +0200

    Scuffed right-to-left version of the algorithm. Now to achieve left-to-right.

--- languages/erlang/basic/sumnsums/src/sumnsums.erl
+++ languages/erlang/basic/sumnsums/src/sumnsums.erl
@@ -5,10 +5,14 @@
 strlist_to_intlist(Str_List) ->
     lists:map(fun (Elem) -> {Num, _} = string:to_integer(Elem), Num end, Str_List).
 
+sum_of_nth_sums([]) -> 0;
+sum_of_nth_sums(Int_List) ->
+    Current_Sum = lists:sum(Int_List),
+    [_|Tail] = Int_List,
+    Current_Sum + sum_of_nth_sums(Tail).
+
 main(Args) ->
-    A = 3,
     Int_Args = strlist_to_intlist(Args),
     io:format("The list of strs from command line: ~p ~n", [Args]),
     io:format("The list of ints: ~w~n", [Int_Args]),
-    io:format("Sum of all the ints is: ~p!~n", [lists:sum(Int_Args)]),
-    io:format("BTW, the variable is ~p! ~n", [A]).
+    io:format("SUM OF NTH SUMS ALREADY POGOMEGA?! => [~p]~n", [sum_of_nth_sums(Int_Args)]).
