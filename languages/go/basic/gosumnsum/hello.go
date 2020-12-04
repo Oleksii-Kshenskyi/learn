d791007e88f7a844f85182dcb6e1769cc82c8017
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2020-12-04 22:46:57 +0200

    Go: sum of nth sums: summing array of numbers implemented.

--- languages/go/basic/gosumnsum/hello.go
+++ languages/go/basic/gosumnsum/hello.go
@@ -20,7 +20,15 @@ func stringArrToIntArr(strarr []string) []int {
 	return result
 }
 
+func sumOnce(sumThis []int) int {
+	acc := 0
+	for _, element := range sumThis {
+		acc += element
+	}
+	return acc
+}
+
 func main() {
 	cliArgs := os.Args[1:]
-	fmt.Println("CLI args provided: ", stringArrToIntArr(cliArgs))
+	fmt.Println("Sum of the args: ", sumOnce(stringArrToIntArr(cliArgs)))
 }
