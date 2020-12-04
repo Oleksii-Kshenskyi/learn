17f3e68c09dfb06dd8c3158cbaad4adc66fcfecc
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2020-12-04 22:57:10 +0200

    Go: sum of nth sums: finished implementation.

--- languages/go/basic/gosumnsum/hello.go
+++ languages/go/basic/gosumnsum/hello.go
@@ -28,7 +28,16 @@ func sumOnce(sumThis []int) int {
 	return acc
 }
 
+func sumOfNthSums(sumThis []int) int {
+	acc := 0
+	for index := 0; index < len(sumThis); index++ {
+		slice := sumThis[:index+1]
+		acc += sumOnce(slice)
+	}
+	return acc
+}
+
 func main() {
 	cliArgs := os.Args[1:]
-	fmt.Println("Sum of the args: ", sumOnce(stringArrToIntArr(cliArgs)))
+	fmt.Println("Sum of the args: ", sumOfNthSums(stringArrToIntArr(cliArgs)))
 }
