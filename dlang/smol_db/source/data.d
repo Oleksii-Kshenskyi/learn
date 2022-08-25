dee374ce12fd02ae4024e4ab56716b9abcbc9e21
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2022-08-25 21:43:45 +0300

    D: smol_db: implemented listing all the games in the database.

--- dlang/smol_db/source/data.d
+++ dlang/smol_db/source/data.d
@@ -1,5 +1,6 @@
 module data;
 
+import std.stdio;
 import d2sqlite3;
 import std.typecons : Nullable;
 
@@ -23,7 +24,25 @@ Statement insert_statement(Database db) {
 
 
 
-void select_and_print(Database db) {}
+void select_and_print(Database db) {
+    auto count = db.execute("SELECT count(*) FROM videogame")
+                   .oneValue!long;
+    writeln("Videogames in the database: ", count);
+
+    auto result = db.execute("SELECT * FROM videogame");
+    writeln("========VIDEOGAMES=========="); writeln();
+    foreach(record; result) {
+        auto id = record.peek!long(0);
+        writeln("Row ID: ", id);
+        writeln("Game Title: ", record["title"].as!string);
+        writeln("Released In: ", record["year"].as!int);
+        writeln("Developer Studio: ", record["developer"].as!string);
+        writeln("Publisher: ", record["publisher"].as!string);
+        writeln("Genre: ", record["genre"].as!string); writeln();
+    }
+    writeln("===========END=============");
+}
+
 void insert_new_game(Database db) {}
 void select_by_name(Database db, string* name) {}
 void select_by_developer(Database db, string* developer) {}
