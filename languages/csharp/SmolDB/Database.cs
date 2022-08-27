43f8c233daa014849105d3f8be0fe979097c90ac
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2022-08-28 01:13:21 +0300

    C#: SmolDB: implemented listing records and writing a new record to the database.

--- languages/csharp/SmolDB/Database.cs
+++ languages/csharp/SmolDB/Database.cs
@@ -3,15 +3,81 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
+using SQLitePCL;
 
 namespace SmolDB
 {
-    static internal class Database
+    internal class DBSchema {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [NotNull]
+        public string? Title { get; set; }
+
+        public ushort Year { get; set; }
+
+        public string? Developer { get; set; }
+
+        public string? Publisher { get; set; }
+
+        public string? Genre { get; set; }
+    }
+    internal class Database
     {
-        public static void ListRecords() { }
-        public static void WriteNewRecord() { }
-        public static void LookupByTitle(string Title) { }
-        public static void LookupByDeveloper(string Developer) { }
-        public static void LookupByPublisher(string Publisher) { }
+        private SQLiteConnection _db;
+
+        public Database(string Folder) {
+            var databasePath = Path.Combine(Folder, "database.db");
+            _db = new SQLiteConnection(databasePath);
+            _db.CreateTable<DBSchema>();
+        }
+        public void ListRecords() {
+            Console.WriteLine("Games in database: " + _db.Table<DBSchema>().Count());
+            Console.WriteLine("===========VIDEOGAMES=========="); Console.WriteLine();
+            var selectAll = _db.Table<DBSchema>();
+            foreach (var record in selectAll) {
+                Console.WriteLine("ID: " + record.Id);
+                Console.WriteLine("Title: " + record.Title);
+                Console.WriteLine("Year: " + record.Year);
+                Console.WriteLine("Developer: " + record.Developer);
+                Console.WriteLine("Publisher: " + record.Publisher);
+                Console.WriteLine("Genre: " + record.Genre);
+                Console.WriteLine();
+            }
+            Console.WriteLine("===========VIDEOGAMES==========");
+        }
+        public void WriteNewRecord() {
+            Console.WriteLine("Please provide info for a new videogame record in the database...");
+            Console.Write("Game's Title? => "); var title = Console.ReadLine();
+
+            ushort year = 0;
+            var intProvided = false;
+            while (!intProvided)
+            {
+                try
+                {
+                    Console.Write("Year of Release? => "); year = Convert.ToUInt16(Console.ReadLine());
+                    intProvided = true;
+                }
+                catch { intProvided = false; Console.WriteLine("You have to provide a number ranging 0-65535 for year of release."); }
+            }
+            
+            Console.Write("Game's Developer? => "); var developer = Console.ReadLine();
+            Console.Write("Game's Publisher? => "); var publisher = Console.ReadLine();
+            Console.Write("Game's Genre? => "); var genre = Console.ReadLine();
+
+            var record = new DBSchema { 
+                Title = title,
+                Year = year,
+                Developer = developer,
+                Publisher = publisher,
+                Genre = genre
+            };
+            _db.Insert(record);
+        }
+        public void LookupByTitle(string Title) { }
+        public void LookupByDeveloper(string Developer) { }
+        public void LookupByPublisher(string Publisher) { }
     }
 }
--- languages/csharp/SmolDB/Program.cs
+++ languages/csharp/SmolDB/Program.cs
@@ -1,12 +1,13 @@
 ﻿using CommandLine;
 using SmolDB;
 
+var db = new Database(".");
 var result = Parser.Default.ParseArguments<Options>(args)
     .WithParsed(o =>
     {
-        if (o.ListRecords) Database.ListRecords();
-        else if (o.WriteNewRecord) Database.WriteNewRecord();
-        else if (o.Title != null) Database.LookupByTitle(o.Title);
-        else if (o.Developer != null) Database.LookupByDeveloper(o.Developer);
-        else if (o.Publisher != null) Database.LookupByPublisher(o.Publisher);
+        if (o.ListRecords) db.ListRecords();
+        else if (o.WriteNewRecord) db.WriteNewRecord();
+        else if (o.Title != null) db.LookupByTitle(o.Title);
+        else if (o.Developer != null) db.LookupByDeveloper(o.Developer);
+        else if (o.Publisher != null) db.LookupByPublisher(o.Publisher);
     });
