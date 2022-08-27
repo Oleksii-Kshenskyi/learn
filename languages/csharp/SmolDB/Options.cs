137d15bc4abfec478671cd06bfccb52e5254842f
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2022-08-27 23:45:37 +0300

    C#: SmolDB: Made the commands mutually exclusive.

--- languages/csharp/SmolDB/Options.cs
+++ languages/csharp/SmolDB/Options.cs
@@ -2,18 +2,18 @@
 
 class Options
 {
-    [Option('l', "list", HelpText = "List all videogames in the database.")]
+    [Option('l', "list", SetName = "List", HelpText = "List all videogames in the database.")]
     public bool ListRecords { get; set; }
 
-    [Option('w', "write", HelpText = "Write info about a new game to the database.")]
+    [Option('w', "write", SetName = "Write", HelpText = "Write info about a new game to the database.")]
     public bool WriteNewRecord { get; set; }
 
-    [Option('t', "title", HelpText = "Lookup games in the database by their title.")]
+    [Option('t', "title", SetName = "Title", HelpText = "Lookup games in the database by their title.")]
     public string? Title { get; set; }
 
-    [Option('d', "developer", HelpText = "Lookup games in the database by their developer studio name.")]
+    [Option('d', "developer", SetName = "Dev", HelpText = "Lookup games in the database by their developer studio name.")]
     public string? Developer { get; set; }
 
-    [Option('p', "publisher", HelpText = "Lookup games in the database by their publisher name.")]
+    [Option('p', "publisher", SetName = "Pub", HelpText = "Lookup games in the database by their publisher name.")]
     public string? Publisher { get; set; }
 }
