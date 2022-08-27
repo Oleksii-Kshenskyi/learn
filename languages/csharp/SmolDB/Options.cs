21228225e4a92f85a0bd9b852e8f40951d83e0bb
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2022-08-27 23:41:48 +0300

    C#: SmolDB: Command Line template for the smol_db D program clone.

---
+++ languages/csharp/SmolDB/Options.cs
@@ -0,0 +1,19 @@
+﻿using CommandLine;
+
+class Options
+{
+    [Option('l', "list", HelpText = "List all videogames in the database.")]
+    public bool ListRecords { get; set; }
+
+    [Option('w', "write", HelpText = "Write info about a new game to the database.")]
+    public bool WriteNewRecord { get; set; }
+
+    [Option('t', "title", HelpText = "Lookup games in the database by their title.")]
+    public string? Title { get; set; }
+
+    [Option('d', "developer", HelpText = "Lookup games in the database by their developer studio name.")]
+    public string? Developer { get; set; }
+
+    [Option('p', "publisher", HelpText = "Lookup games in the database by their publisher name.")]
+    public string? Publisher { get; set; }
+}
--- languages/csharp/SmolDB/Program.cs
+++ languages/csharp/SmolDB/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CommandLine;
+
+var result = Parser.Default.ParseArguments<Options>(args)
+    .WithParsed(o =>
+    {
+        Console.WriteLine("list? " + o.ListRecords);
+        Console.WriteLine("write? " + o.WriteNewRecord);
+        Console.WriteLine("bytitle? " + (o.Title != null ? o.Title : "null"));
+        Console.WriteLine("bydev? " + (o.Developer != null ? o.Developer : "null"));
+        Console.WriteLine("bypub? " + (o.Publisher != null ? o.Publisher : "null"));
+    });
