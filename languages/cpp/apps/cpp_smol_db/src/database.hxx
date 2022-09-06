1d8e01d27827256a517fa57e2d93650cb073f7e5
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2022-09-06 23:47:35 +0300

    C++: cpp_smol_db: Implemented lookup by title, dev, pub.

--- languages/cpp/apps/cpp_smol_db/src/database.hxx
+++ languages/cpp/apps/cpp_smol_db/src/database.hxx
@@ -61,9 +61,86 @@ class Database {
 
             this->_db << "INSERT INTO videogame(title, year, developer, publisher, genre) VALUES (:title, :year, :developer, :publisher, :genre)", use(title), use(year), use(developer), use(publisher), use(genre);
         }
-        void lookup_by_title(const std::string& title) { (void) title; }
-        void lookup_by_developer(const std::string& developer) { (void) developer; }
-        void lookup_by_publisher(const std::string& publisher) { (void) publisher; }
+        void lookup_by_title(const std::string& title) { 
+            int games_count = 0;
+            this->_db << "SELECT count(*) FROM videogame WHERE title LIKE '%" << title << "%'", into(games_count);
+            std::cout << "Games in the database with title [" << title << "]: " << games_count << std::endl;
+
+            std::vector<int> ids; ids.resize(100);
+            std::vector<std::string> titles; titles.resize(100);
+            std::vector<int> years; years.resize(100);
+            std::vector<std::string> developers; developers.resize(100);
+            std::vector<std::string> publishers; publishers.resize(100);
+            std::vector<std::string> genres; genres.resize(100);
+            this->_db << "SELECT * FROM videogame WHERE title LIKE '%" << title << "%'", into(ids), into(titles), into(years), into(developers), into(publishers), into(genres);
+
+            std::cout << "===========SEARCH BY TITLE==========" << std::endl << std::endl;
+            for(std::size_t i = 0; i < ids.size(); i++) {
+                std::cout << "Videogame {" << std::endl;
+                std::cout << "    ID: '" << ids[i] << "'," << std::endl;
+                std::cout << "    Game's title: '" << titles[i] << "'," << std::endl;
+                std::cout << "    Game's year of release: '" << years[i] << "'," << std::endl;
+                std::cout << "    Game's developer: '" << developers[i] << "'," << std::endl;
+                std::cout << "    Game's publisher: '" << publishers[i] << "'," << std::endl;
+                std::cout << "    Game's genre: '" << genres[i] << "'," << std::endl;
+                std::cout << "}" << std::endl << std::endl;
+            }
+            std::cout << "========END SEARCH BY TITLE=========" << std::endl;
+        }
+
+        void lookup_by_developer(const std::string& developer) {
+            int games_count = 0;
+            this->_db << "SELECT count(*) FROM videogame WHERE developer LIKE '%" << developer << "%'", into(games_count);
+            std::cout << "Games in the database by developer studio [" << developer << "]: " << games_count << std::endl;
+
+            std::vector<int> ids; ids.resize(100);
+            std::vector<std::string> titles; titles.resize(100);
+            std::vector<int> years; years.resize(100);
+            std::vector<std::string> developers; developers.resize(100);
+            std::vector<std::string> publishers; publishers.resize(100);
+            std::vector<std::string> genres; genres.resize(100);
+            this->_db << "SELECT * FROM videogame WHERE developer LIKE '%" << developer << "%'", into(ids), into(titles), into(years), into(developers), into(publishers), into(genres);
+
+            std::cout << "=============SEARCH BY DEV=============" << std::endl << std::endl;
+            for(std::size_t i = 0; i < ids.size(); i++) {
+                std::cout << "Videogame {" << std::endl;
+                std::cout << "    ID: '" << ids[i] << "'," << std::endl;
+                std::cout << "    Game's title: '" << titles[i] << "'," << std::endl;
+                std::cout << "    Game's year of release: '" << years[i] << "'," << std::endl;
+                std::cout << "    Game's developer: '" << developers[i] << "'," << std::endl;
+                std::cout << "    Game's publisher: '" << publishers[i] << "'," << std::endl;
+                std::cout << "    Game's genre: '" << genres[i] << "'," << std::endl;
+                std::cout << "}" << std::endl << std::endl;
+            }
+            std::cout << "===========END SEARCH BY DEV============" << std::endl;
+        }
+
+        void lookup_by_publisher(const std::string& publisher) { (void) publisher;
+            int games_count = 0;
+            this->_db << "SELECT count(*) FROM videogame WHERE publisher LIKE '%" << publisher << "%'", into(games_count);
+            std::cout << "Games in the database by publisher [" << publisher << "]: " << games_count << std::endl;
+
+            std::vector<int> ids; ids.resize(100);
+            std::vector<std::string> titles; titles.resize(100);
+            std::vector<int> years; years.resize(100);
+            std::vector<std::string> developers; developers.resize(100);
+            std::vector<std::string> publishers; publishers.resize(100);
+            std::vector<std::string> genres; genres.resize(100);
+            this->_db << "SELECT * FROM videogame WHERE publisher LIKE '%" << publisher << "%'", into(ids), into(titles), into(years), into(developers), into(publishers), into(genres);
+
+            std::cout << "==============SEARCH BY PUB==============" << std::endl << std::endl;
+            for(std::size_t i = 0; i < ids.size(); i++) {
+                std::cout << "Videogame {" << std::endl;
+                std::cout << "    ID: '" << ids[i] << "'," << std::endl;
+                std::cout << "    Game's title: '" << titles[i] << "'," << std::endl;
+                std::cout << "    Game's year of release: '" << years[i] << "'," << std::endl;
+                std::cout << "    Game's developer: '" << developers[i] << "'," << std::endl;
+                std::cout << "    Game's publisher: '" << publishers[i] << "'," << std::endl;
+                std::cout << "    Game's genre: '" << genres[i] << "'," << std::endl;
+                std::cout << "}" << std::endl << std::endl;
+            }
+            std::cout << "============END SEARCH BY PUB=============" << std::endl;
+        }
 };
 
 #endif // DATABASE_HXX
