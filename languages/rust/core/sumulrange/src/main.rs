5246a94f74d581000cba313165652577c1ec7d01
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2022-09-11 01:43:35 +0300

    Rust: short project manipulating a range of ints.

---
+++ languages/rust/core/sumulrange/src/main.rs
@@ -0,0 +1,8 @@
+use std::env;
+
+fn main() {
+    let myargs: Vec<String> = env::args().collect::<Vec<_>>();
+    let num = myargs.get(1).unwrap().parse::<i64>().unwrap();
+
+    println!("The num: {}", (0..num).map(|x| x * 2).sum::<i64>());
+}
