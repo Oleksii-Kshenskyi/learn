250f66b0ac662a68f02d44a432a109d5d9accc5d
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2023-03-19 15:46:00 +0200

    C++: TODO: read and write commands ready to use.

--- languages/cpp/apps/todo-demo/src/cli/main.cxx
+++ languages/cpp/apps/todo-demo/src/cli/main.cxx
@@ -1,8 +1,6 @@
 #include "lib/system.hxx"
 #include "lib/test.hxx"
 
-// TODO: Should write a new note into the file
-
 // TODO: refactor/decouple code into the lib and their own classes
 
 int main(int argc, char** argv) {
@@ -13,25 +11,25 @@ int main(int argc, char** argv) {
         return 1;
     }
     if(args[1] == "read") {
-        std::cout << "READ!" << std::endl;
-        return 0;
+        std::vector<std::string> todos;
+        std::ifstream todo_file("todo.txt", std::ios::in);
+        std::string line;
+        while(std::getline(todo_file, line)) {
+            todos.push_back(line);
+        }
+
+        std::copy(todos.begin(), todos.end(), std::ostream_iterator<std::string>(std::cout, "; "));
+        std::cout << std::endl;
     } else if(args[1] == "write") {
-        std::cout << "WRITE!" << std::endl;
-        return 0;
+        if(args.size() != 3) {
+            std::cout << "ERROR: the `write` command is expecting exactly one argument, a new TODO note." << std::endl;
+            return 1;
+        }
+        std::ofstream todo_file("todo.txt", std::ios::app);
+        todo_file << args[2] << "\n";
     } else {
         std::cout << "ERROR: " << args[1] << ": unknown command." << std::endl;
         return 1;
     }
-
-    std::vector<std::string> todos;
-    std::ifstream todo_file("todo.txt", std::ios::in);
-    std::string line;
-    while(std::getline(todo_file, line)) {
-        todos.push_back(line);
-    }
-    
-    std::copy(todos.begin(), todos.end(), std::ostream_iterator<std::string>(std::cout, "; "));
-    std::cout << std::endl;
-
     return 0;
 }
