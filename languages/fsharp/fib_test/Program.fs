558ad5e3bb327ab7d855a7193b5e3c254e6209a8
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2024-03-28 19:24:46 +0200

    F#: modified Fibonacci according to MS advice, somehow this makes it faster than pure C...

--- languages/fsharp/fib_test/Program.fs
+++ languages/fsharp/fib_test/Program.fs
@@ -1,6 +1,10 @@
-﻿let rec fib n = match n with
-                | 0 -> 0
-                | 1 -> 1
-                | n -> fib (n - 1) + fib (n - 2)
+﻿let fib n =
+    let rec loop acc1 acc2 n =
+        match n with
+        | 0 -> acc1
+        | 1 -> acc2
+        | _ ->
+            loop acc2 (acc1 + acc2) (n - 1)
+    loop 0 1 n
 
 printfn $"F# fib {42} is {fib 42}"
